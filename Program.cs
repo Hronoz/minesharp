e86b60e97ef77d6e9b132f562d5a90c84807af2d
Hronoz <hronozovichhronoz@gmail.com>
2023-07-08 15:17:33 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Minesharp;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
+namespace minesharp;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var nativeWindowSettings = new NativeWindowSettings()
+            {
+                Size = new Vector2i(800, 600),
+                Title = "LearnOpenTK - Camera",
+                // This is needed to run on macos
+                Flags = ContextFlags.ForwardCompatible,
+            };
+
+            using (var window = new Window(GameWindowSettings.Default, nativeWindowSettings))
+            {
+                window.Run();
+            }
+    }
+}
